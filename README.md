d75841d8445f6a3378c6478c724a351801d450b8
stecov <stefan.cova@publicis-modem.fr>
2015-02-16 14:20:44 +0100

    refactor code

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 SelfieMathon
 ===============
 
-NodeJS app to take a photo and to post it to the wall of fame.
+NodeJS app to take a photo from a video stream and to post it to the wall of fame.
 Works on Chrome, Firefox and Android
-
-How to install
-===============
-- Clone the repo
-- Npm install
-- cd app
-- node app
--- app/js/client.js
+++ app/js/client.js
@@ -1,15 +1,154 @@
-// Define websocket
-var socket = io.connect('http://pm-stecov.equesto.fr:8080');
-
+// Variables
+var socket = '';
 var $wall = '';
 var $progress = '';
 var users = [];
 var user = '';
+var canvas = '';
+var context = '';
+var video = '';
+
+
+$(document).ready(function () {
+
+    initWebsockets();
+
+    // Get Dom elements
+    $wall = $("#wall");
+    $progress = $("#progress");
+
+    captureVideo();
+    sendPhoto();
+});
+
+
+/**
+ * initWebsockets
+ */
+var initWebsockets = function () {
+
+    // Define websocket
+    socket = io.connect('http://pm-stecov.equesto.fr:8080') //TODO: add settings for this;
+
+    // IO : Read images from server
+    socket.on('readfiles', function (imagesArray) {
+        console.log('on_readfiles', imagesArray);
+
+        for (var i = 0; i < imagesArray.length; i++) {
+            insertImage(imagesArray[i], 'after')
+        }
+    });
+
+    // IO : Save a new file
+    socket.on('filesaved', function (image) {
+        console.log('on_filesaved', image);
+
+        insertImage(image, 'before');
+        $progress.removeClass('active');
+    });
+};
+
+
+/**
+ * captureVideo
+ */
+var captureVideo = function () {
+
+    // Grab elements, create settings, etc.
+    canvas = document.getElementById("preview");
+    context = canvas.getContext("2d");
+    video = document.getElementById("video");
+    var videoObj = {
+        "video": true,
+        "videoWidth": 200,
+        "videoHeight": 200
+
+    },
+    errBack = function (error) {
+        console.log("Video capture error: ", error.code);
+    };
+
+    // Put video listeners into place
+    if (navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function (stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(videoObj, function (stream) {
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+    else if (navigator.mozGetUserMedia) { // Firefox-prefixed
+        navigator.mozGetUserMedia(videoObj, function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+};
+
+/**
+ * cropVideo
+ * Crop video do a square depending of the orientation of the camera
+ */
+var cropVideo = function () {
+
+    // Set canvas size to a square
+    var canvasSize = Math.min(video.videoWidth, video.videoHeight);
+    var cropX = 0;
+    var cropY = 0;
+    // Define crop
+    if (video.videoWidth > video.videoHeight) {
+        cropX = video.videoWidth - video.videoHeight;
+    } else {
+        cropY = video.videoHeight - video.videoWidth;
+    }
+    console.log('canvasSize', canvasSize, 'cropX', cropX / 2, 'cropY', cropY / 2);
+
+
+    // Set canvas canvasSize and draw video in canvas with cropping
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(video, -cropX, -cropY, canvasSize + cropX, canvasSize + cropY);
+
+};
+
+
+/**
+ * sendPhoto
+ */
+var sendPhoto = function () {
+
+    $("#form").on("submit", function (e) {
+
+        e.preventDefault();
+
+        // Show progress bar
+        $progress.addClass("active");
+
+        cropVideo();
+
+        // Check user
+        var pseudo = $("#pseudo").val();
+        if (isUserExist(pseudo)) {
+            removeItemInArray(users, pseudo);
+            $('#'+pseudo).remove();
+        }
+        console.log('submit', users, pseudo);
+
+        // Send canvas to server
+        var dataURL = canvas.toDataURL();
+        socket.emit('newimage', {image: dataURL, pseudo: pseudo})
+
+    });
+};
 
 
 /**
  * Test if an user exist
- * @param user
+ * @param user {string}
  * @returns {boolean}
  */
 var isUserExist = function (user) {
@@ -24,12 +163,13 @@ var isUserExist = function (user) {
     }
 };
 
+
 /**
  * Remove an item from an array
  * @param arr
  * @param item
  */
-var removeInArray = function (arr, item) {
+var removeItemInArray = function (arr, item) {
     for (var i = arr.length; i--;) {
         if (arr[i] === item) {
             arr.splice(i, 1);
@@ -37,10 +177,11 @@ var removeInArray = function (arr, item) {
     }
 };
 
+
 /**
  * Insert image in DOM
  * @param imagePath {string}
- * @param method {string} appendChild or insertBefore
+ * @param method {string} : insert after or before
  */
 var insertImage = function (imagePath, method) {
     console.log('insertImage', imagePath, method);
@@ -68,110 +209,3 @@ var insertImage = function (imagePath, method) {
     // Add user to array users
     users.push(imagePath.replace('.png', ''));
 };
-
-
-// IO: Read images from server
-socket.on('readfiles', function (imagesArray) {
-    console.log('on_readfiles', imagesArray);
-
-    for (var i = 0; i < imagesArray.length; i++) {
-        insertImage(imagesArray[i], 'after')
-    }
-});
-
-
-// IO: Save a new file
-socket.on('filesaved', function (image) {
-    console.log('on_filesaved', image);
-
-    insertImage(image, 'before');
-    $progress.removeClass('active');
-});
-
-
-// ############ CAPTURE CAMERA ###########/
-
-// On dom ready
-$(document).ready(function () {
-    console.log("ready!");
-
-    $wall = $("#wall");
-    $progress = $("#progress");
-
-    // Grab elements, create settings, etc.
-    var canvas = document.getElementById("preview"),
-        context = canvas.getContext("2d"),
-        video = document.getElementById("video"),
-        videoObj = {
-            "video": true,
-            "videoWidth": 200,
-            "videoHeight": 200
-
-        },
-        errBack = function (error) {
-            console.log("Video capture error: ", error.code);
-        };
-
-    // Put video listeners into place
-    if (navigator.getUserMedia) { // Standard
-        navigator.getUserMedia(videoObj, function (stream) {
-            video.src = stream;
-            video.play();
-        }, errBack);
-    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
-        navigator.webkitGetUserMedia(videoObj, function (stream) {
-            video.src = window.webkitURL.createObjectURL(stream);
-            video.play();
-        }, errBack);
-    }
-    else if (navigator.mozGetUserMedia) { // Firefox-prefixed
-        navigator.mozGetUserMedia(videoObj, function (stream) {
-            video.src = window.URL.createObjectURL(stream);
-            video.play();
-        }, errBack);
-    }
-
-
-    // Send photo
-    $("#form").on("submit", function (e) {
-
-        e.preventDefault();
-
-        // Set canvas size to a square
-        var canvasSize = Math.min(video.videoWidth, video.videoHeight);
-        var cropX = 0;
-        var cropY = 0;
-        // Define crop
-        if (video.videoWidth > video.videoHeight) {
-            cropX = video.videoWidth - video.videoHeight;
-        } else {
-            cropY = video.videoHeight - video.videoWidth;
-        }
-        console.log('canvasSize', canvasSize, 'cropX', cropX / 2, 'cropY', cropY / 2);
-
-
-        // Set canvas canvasSize and draw video in canvas with cropping
-        canvas.width = canvasSize;
-        canvas.height = canvasSize;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(video, -cropX, -cropY, canvasSize + cropX, canvasSize + cropY);
-
-        // Show progress bar
-        $progress.addClass("active");
-
-        // Send canvas to server
-        var dataURL = canvas.toDataURL();
-        var pseudo = $("#pseudo").val();
-
-        console.log('submit', users, pseudo);
-
-        if (isUserExist(pseudo)) {
-            removeInArray(users, pseudo);
-            $(pseudo).remove();
-        }
-
-        socket.emit('newimage', {image: dataURL, pseudo: pseudo})
-
-    });
-
-});
